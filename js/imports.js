263688d805a3fce8c3ac8b6dcb93e5011423e4d0
NikolasifyArt <bigbosssnakeify@gmail.com>
2024-11-20 18:21:02 +0200

    Add initial project files and configuration

---
+++ js/imports.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import axios from 'axios';
+import { gsap } from 'gsap';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faFacebookF, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+// Importing necessary third-party packages
+
+// Importing jQuery for DOM manipulation
+
+// Importing Lodash for utility functions
+
+// Importing Axios for making HTTP requests
+
+// Importing GSAP for animations
+
+// Importing FontAwesome for icons
+
+// Adding FontAwesome icons to the library
+library.add(faFacebookF, faInstagram, faGithub);
+dom.watch();
