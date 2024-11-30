ce35db3a0c3a21c6ee21f5e338931493657e5944
NikolasifyArt <bigbosssnakeify@gmail.com>
2024-11-30 18:34:24 +0200

    Remove deprecated Netlify function files and related TypeScript definitions

--- netlify/functions/subformJs.js
+++
@@ -1,8 +0,0 @@
-export async function handler(event, context) {
-    const data = JSON.parse(event.body);
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Form submitted successfully' }),
-    };
-}
